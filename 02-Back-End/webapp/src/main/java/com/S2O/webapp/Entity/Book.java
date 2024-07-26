90120a16cbd6ea8842f772aeb520517604017b20
MfmRifath <mmfmrifath@gmail.com>
2024-07-26 22:16:08 +0530

    complete Book entity

--- 02-Back-End/webapp/src/main/java/com/S2O/webapp/Entity/Book.java
+++ 02-Back-End/webapp/src/main/java/com/S2O/webapp/Entity/Book.java
@@ -1,25 +1,37 @@
 package com.S2O.webapp.Entity;
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "author")
     private String author;
+    @Column(name = "cover_image")
     private String coverImage;
+    @Column(name = "discription")
     private String description;
+    @Column(name = "publisher")
     private String publisher;
+    @Column(name = "date")
     private String publicationDate;
-    private String isbn;
+    @Column(name = "genre")
     private String genre;
+    @Column(name = "pages")
     private int pages;
+    @Column(name = "read_online_link")
     private String readOnlineLink;
-    private String downloadEpubLink;
 
     @Lob
-    @Column(columnDefinition = "MEDIUMBLOB")
+    @Column(columnDefinition = "MEDIUMBLOB",name = "book_pd")
     private byte[] pdfFile;
 
     // Getters and setters
